680551941b7990bffadeb85c044fa82ec1b6c86a
emreiser <emilie.reiser@gmail.com>
2014-10-07 16:38:40 -0400

    Implement survey status change

--- lib/api.rb
+++ lib/api.rb
@@ -9,6 +9,7 @@ class Survey
 
   key :survey_id, Integer
   key :title, String
+  key :status, String
   key :inputs, Array
 end
 
@@ -30,6 +31,36 @@ class PTApi < Sinatra::Base
     headers 'Access-Control-Allow-Origin' => '*'
   end
 
+  get '/surveys' do
+    content_type :json
+    Survey.all.to_json
+  end
+
+  post '/surveys' do
+    data = JSON.parse(request.body.read)
+    survey = Survey.create(data)
+    survey.status = 'active'
+    survey.save
+    survey.to_json
+  end
+
+  get '/surveys/:id' do
+    content_type :json
+    Survey.first(_id: params[:id].to_i).to_json
+  end
+
+  get '/surveys/:id/close' do
+    survey = Survey.first(_id: params[:id].to_i)
+    survey.status = 'closed'
+    survey.save
+    survey.to_json
+  end
+
+  get '/surveys/:id/responses' do
+    content_type :json
+    Response.all(survey_id: params[:id].to_i).to_json
+  end
+
   get '/responses' do
     content_type :json
     Response.all.to_json
@@ -38,26 +69,6 @@ class PTApi < Sinatra::Base
   post '/responses' do
     response_data = JSON.parse(params[:response])
     Response.create(response_data)
-  end
-
-  get '/surveys' do
-    content_type :json
-    Survey.all.to_json
-  end
-
-  get '/surveys/:id' do
-    content_type :json
-    Survey.first(_id: params[:id].to_i).to_json
-  end
-
-  get '/surveys/:id/responses' do
-    content_type :json
-    Response.all(survey_id: params[:id].to_i).to_json
-  end
-
-  post '/surveys' do
-    data = JSON.parse(request.body.read)
-    Survey.create(data)
   end
 
 end
